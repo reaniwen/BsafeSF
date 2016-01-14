c0969fd6dd14b315aa2e9f5a26c182105814e0ce
reaniwen <reaniwen@gmail.com>
2016-01-13 23:53:43 -0500

    map shown

--- BsafeSF/ViewController.swift
+++ BsafeSF/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import MapKit
+
+private let initLocation = CLLocation(latitude: 37.7545565620279, longitude: -122.419711251166)
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var baseMapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setMapRegion(initLocation)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func setMapRegion(location: CLLocation) {
+        let regionRadius: CLLocationDistance = 12000
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+            regionRadius * 2.0, regionRadius * 2.0)
+        self.baseMapView.setRegion(coordinateRegion, animated: true)
+    }
 
 }
 
